721246efce21c7a8b7454771ed748c84ddb7b579
Mads <mach@mercantec.dk>
2018-01-28 13:25:53 +0100

    fixed splitview controller

---
+++ Tyme/CustomSplitViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CustomSplitViewController.swift
+//  Tyme
+//
+//  Created by elev on 28/01/2018.
+//  Copyright © 2018 Mads Bock. All rights reserved.
+//
+
+import UIKit
+
+class CustomSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.preferredDisplayMode = .allVisible
+        // Do any additional setup after loading the view.
+    }
+
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController) -> Bool {
+        // Return true to prevent UIKit from applying its default behavior
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
